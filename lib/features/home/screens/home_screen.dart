83937b7c6a815b262ed6bbf72a9d3225136cc84b
elikenny333 <elikenny333@gmail.com>
2025-08-30 19:42:01 +0000

    feat: Implement edit deal functionality

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,9 +1,38 @@
+
 import 'package:flutter/material.dart';
-import 'package:myapp/features/home/widgets/deal_card.dart';
+import 'package:deal_diver/features/home/models/deal.dart';
+import 'package:deal_diver/features/home/widgets/deal_card.dart';
 import '../../../app/theme/app_text_styles.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<Deal> deals;
+
+  @override
+  void initState() {
+    super.initState();
+    deals = List.generate(
+      10,
+      (index) => Deal(
+        title: 'Futuristic Gadget Deal ${index + 1}',
+        description: 'Save 50% on this amazing piece of tech. Limited time offer, dive into the deal now!',
+        price: 99.99 + (index * 10),
+        image: 'https://picsum.photos/400/200?random=${index}',
+      ),
+    );
+  }
+
+  void _updateDeal(int index, Deal updatedDeal) {
+    setState(() {
+      deals[index] = updatedDeal;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +45,14 @@ class HomeScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemCount: 10, // Mock data
+        itemCount: deals.length,
         itemBuilder: (context, index) {
-          return const DealCard();
+          return DealCard(
+            deal: deals[index],
+            onDealUpdated: (updatedDeal) {
+              _updateDeal(index, updatedDeal);
+            },
+          );
         },
       ),
     );
